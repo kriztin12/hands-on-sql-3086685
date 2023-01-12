4558bab669cb5f5e0dabe6d3396dce0c3f83ee48
kriztin12 <61216182+kriztin12@users.noreply.github.com>
2023-01-12 02:51:29 +0000

    where conditions 01_02

--- queries/01_02/ComparisonConditions.sql
+++ queries/01_02/ComparisonConditions.sql
@@ -2,11 +2,15 @@ SELECT empno, name,hiredate
 FROM emp_tab 
 WHERE name LIKE '_D%'; 
 
+SELECT empno, name,hiredate 
+FROM emp_tab 
+WHERE name LIKE '%D%'; 
+
 SELECT * FROM emp_tab 
 WHERE deptno IN (30,40);
 
 SELECT * FROM emp_tab 
 WHERE salary BETWEEN 2000 AND 6000;
 
-SELECT empno FROM emp_tab 
+SELECT empno, name FROM emp_tab 
 WHERE manager IS NULL;
--- queries/01_02/WHEREcondition.sql
+++ queries/01_02/WHEREcondition.sql
@@ -5,3 +5,5 @@ WHERE salary>2500;
 SELECT empno,name, salary 
 FROM emp_tab 
 WHERE name='KING';
+
+select empno, name from emp_tab where empno = '7007';
